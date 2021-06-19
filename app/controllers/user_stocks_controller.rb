bc75f7fda0d70fc8ded854b3725885a3b5e2cc2a
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-06-19 17:50:21 +0800

    commit before editing

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,6 +1,8 @@
 class UserStocksController < ApplicationController
     def create
-
+      if current_user.buyer?
+        byebug
+      else
         stock = Stock.check_db(params[:ticker])
         if stock.blank?
           stock = Stock.new_lookup(params[:ticker])
@@ -9,6 +11,8 @@ class UserStocksController < ApplicationController
         @user_stock = UserStock.create(user: current_user, stock: stock)
         flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
         redirect_to my_portfolio_path
+      end
+
 
       
     end
